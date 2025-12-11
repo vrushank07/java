dc18fba62b47e921478506e0cc6f03b08d3fd4fa
vrushank07 <vrushankpilli2005@gmail.com>
2025-12-11 16:04:22 +0530

    Add files via upload

---
+++ seventable.java
@@ -0,0 +1,8 @@
+public class seventable {
+    public static void main(String[] args) {
+        int num = 7;
+        for(int i=1; i<=5; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
