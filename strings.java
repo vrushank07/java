c4dacbcc70ca84e3a0acd0ce005d13e7ea44de2d
vrushank07 <vrushankpilli2005@gmail.com>
2025-12-12 11:54:18 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,40 @@
+/*public class strings {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder
+("Hello");
+        sb.append(" World");
+        System.out.println(sb.toString());
+        }
+    }
+    
+
+public class strings {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder
+("Hello");
+        sb.append(" World");
+        System.out.println(sb.reverse());
+        }
+    }
+        
+public class strings {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder
+("Hello");
+        sb.append(" World");
+        System.out.println(sb.replace(0, 5, "Hi"));
+
+        }
+    }
+
+        */
+
+public class strings {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder
+("Hello");
+        sb.append(" World");
+        System.out.println(sb.insert(5, " Java"));
+
+        }
+    }
