6921230dfd9d433d4153cc893b61b690edbc3612
vrushank07 <vrushankpilli2005@gmail.com>
2025-12-15 12:31:40 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,11 @@
+public class arrays {
+    public static void main(String[] args) {
+ String [] subjects = {"Math", "Science", "History", "Art", "Physical Education" , "music", "Computer Science", "Biology", "Chemistry", "Physics"}; 
+ System.out.println("First subject: ");
+ System.out.println(subjects[0]);
+ System.out.println("Last subject: ");
+ subjects[0]="New Subject 1";
+System.out.println(subjects[9]);
+   
+        }
+    }
