630126e62aa8b15f07c6416caa6dd82e42d8c845
vrushank07 <vrushankpilli2005@gmail.com>
2025-12-20 10:29:51 +0530

    Add files via upload

---
+++ findprimenumbersusingsiers.java
@@ -0,0 +1,29 @@
+public class findprimenumbersusingsiers {
+    public static void main(String[] args) {
+        int limit = 50; 
+        boolean[] isPrime = new boolean[limit + 1];
+
+        for (int i = 2; i <= limit; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int p = 2; p * p <= limit; p++) {
+            
+            if (isPrime[p]) {
+             
+                for (int i = p * p; i <= limit; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+
+       
+        System.out.println("Prime numbers up to " + limit + ":");
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    
+}
